a07a86c37d26e71f44fa36e3165c4e24e38533f7
Nooruddin143 <nooruddinmohammadi63@gmail.com>
2024-02-27 11:12:11 -0800

    Initial Commit

---
+++ myGitHubTest/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  myGitHubTest
+//
+//  Created by Nooruddin Mohammadi on 2/27/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
