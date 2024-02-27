ee3196473de3744e918b9de9195b3a64b6776f04
Nooruddin143 <nooruddinmohammadi63@gmail.com>
2024-02-27 11:11:29 -0800

    new file 1

--- incomeTaxForm/otherTest.cpp
+++ incomeTaxForm/otherTest.cpp
@@ -5,4 +5,12 @@
 //  Created by Nooruddin Mohammadi on 2/27/24.
 //
 
-#include "otherTest.hpp"
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
