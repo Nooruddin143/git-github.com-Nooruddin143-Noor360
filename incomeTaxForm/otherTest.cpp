e28928babb845270e54ecad59d3eb76d60f99731
Nooruddin143 <nooruddinmohammadi63@gmail.com>
2024-02-27 11:10:28 -0800

    new file

---
+++ incomeTaxForm/otherTest.cpp
@@ -0,0 +1,8 @@
+//
+//  otherTest.cpp
+//  incomeTaxForm
+//
+//  Created by Nooruddin Mohammadi on 2/27/24.
+//
+
+#include "otherTest.hpp"
---
+++ incomeTaxForm/otherTest.hpp
@@ -0,0 +1,13 @@
+//
+//  otherTest.hpp
+//  incomeTaxForm
+//
+//  Created by Nooruddin Mohammadi on 2/27/24.
+//
+
+#ifndef otherTest_hpp
+#define otherTest_hpp
+
+#include <stdio.h>
+
+#endif /* otherTest_hpp */
